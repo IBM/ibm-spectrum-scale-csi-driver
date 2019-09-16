9a80478f4df9ad8a93a1fcb2e53f4113cda903bf
Smita J. Raut <smita.raut@in.ibm.com>
2019-09-16 14:39:08 +0530

    - Install helper script (#8)
    * - Install helper script
    - Re-organized example files
    
    * - Install helper script
    - Re-organized example files
    
    * Added IMAGES section

--- deploy/classic/create.sh
+++ deploy/classic/create.sh
@@ -3,7 +3,6 @@ kubectl apply -f deploy/common/csi-nodeplugin-rbac.yaml
 kubectl apply -f deploy/common/csi-provisioner-rbac.yaml
 
 kubectl apply -f deploy/classic/spectrum-scale-secret.json
-#kubectl create configmap cert1 --from-file=mycertificate.pem=deploy/classic/mycertificate.pem
 kubectl create configmap spectrum-scale-config --from-file=spectrum-scale-config.json=deploy/classic/spectrum-scale-config.json
 
 kubectl apply -f deploy/common/csi-plugin-attacher.yaml
