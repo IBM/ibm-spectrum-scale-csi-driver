0ad64c6f6a1d9e220e780b0a792913ff1a328456
mavin6618 <christopher.sansone@ibm.com>
2019-11-01 14:29:05 -0700

    Modify parameters to be camel case for both general configuration and storage class configuration

--- csiplugin/settings/scale_config.go
+++ csiplugin/settings/scale_config.go
@@ -17,116 +17,145 @@
 package settings
 
 import (
-        "fmt"
-        "strings"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"path"
-        "io/ioutil"
-        "encoding/json"
+	"strings"
 
-        "github.com/golang/glog"
+	"github.com/golang/glog"
 )
 
 type ScaleSettingsConfigMap struct {
-    Clusters []Clusters
+	Clusters []Clusters
 }
 
-type Primary struct { 
-    PrimaryFS string `json:"primaryFS"` 
-    PrimaryFset string `json:"primaryFset"` 
-    PrimaryCid  string `json:"primaryCid"`
-    InodeLimit  string `json:"inode-limit"`
-    RemoteCluster string `json:"remoteCluster"`
-    RemoteFS string `json:"remoteFS"`
+type Primary struct {
+	PrimaryFSDep  string `json:"primaryFS"` // Deprecated
+	PrimaryFs     string `json:"primaryFs"`
+	PrimaryFset   string `json:"primaryFset"`
+	PrimaryCid    string `json:"primaryCid"`
+	InodeLimitDep string `json:"inode-limit"` // Deprecated
+	InodeLimits   string `json:"inodeLimit"`
+	RemoteCluster string `json:"remoteCluster"`
+	RemoteFSDep   string `json:"remoteFS"` // Deprecated
+	RemoteFs      string `json:"remoteFs"`
 
-    PrimaryFSMount string
-    PrimaryFsetLink string
-    SymlinkAbsolutePath string
-    SymlinkRelativePath string
-} 
-
-type RestAPI struct { 
-    GuiHost string `json:"guiHost"` 
-    GuiPort int `json:"guiPort"` 
-} 
-
-type Clusters struct { 
-    ID string `json:"id"` 
-    Primary Primary `json:"primary,omitempty"` 
-    SecureSslMode bool `json:"secureSslMode"` 
-    Cacert string `json:"cacert"` 
-    Secrets string `json:"secrets"` 
-    RestAPI []RestAPI `json:"restApi"` 
-
-    MgmtUsername string
-    MgmtPassword string
-    CacertValue []byte
+	PrimaryFSMount      string
+	PrimaryFsetLink     string
+	SymlinkAbsolutePath string
+	SymlinkRelativePath string
 }
 
-const ( 
-        DefaultGuiPort int = 443
-        GuiProtocol string = "https"
-        ConfigMapFile string = "/var/lib/ibm/config/spectrum-scale-config.json"
-	SecretBasePath string = "/var/lib/ibm/"
+/* To support backwards compatibility if the PrimaryFs field is not defined then
+   use the previous version of the field. */
+func (primary Primary) GetPrimaryFs() string {
+	if primary.PrimaryFs == "" {
+		return primary.PrimaryFSDep
+	}
+	return primary.PrimaryFs
+}
+
+/* To support backwards compatibility if the RemoteFs field is not defined then
+   use the previous version of the field. */
+func (primary Primary) GetRemoteFs() string {
+	if primary.RemoteFs == "" {
+		return primary.RemoteFSDep
+	}
+	return primary.RemoteFs
+}
+
+/* To support backwards compatibility if the InodeLimit field is not defined then
+   use the previous version of the field. */
+func (primary Primary) GetInodeLimit() string {
+	if primary.InodeLimits == "" {
+		return primary.InodeLimitDep
+	}
+	return primary.InodeLimits
+}
+
+type RestAPI struct {
+	GuiHost string `json:"guiHost"`
+	GuiPort int    `json:"guiPort"`
+}
+
+type Clusters struct {
+	ID            string    `json:"id"`
+	Primary       Primary   `json:"primary,omitempty"`
+	SecureSslMode bool      `json:"secureSslMode"`
+	Cacert        string    `json:"cacert"`
+	Secrets       string    `json:"secrets"`
+	RestAPI       []RestAPI `json:"restApi"`
+
+	MgmtUsername string
+	MgmtPassword string
+	CacertValue  []byte
+}
+
+const (
+	DefaultGuiPort  int    = 443
+	GuiProtocol     string = "https"
+	ConfigMapFile   string = "/var/lib/ibm/config/spectrum-scale-config.json"
+	SecretBasePath  string = "/var/lib/ibm/"
 	CertificatePath string = "/var/lib/ibm/ssl/public"
 )
 
 func LoadScaleConfigSettings() ScaleSettingsConfigMap {
 	glog.V(5).Infof("scale_config LoadScaleConfigSettings")
 
-        file, e := ioutil.ReadFile(ConfigMapFile) // TODO
-        if e != nil {
-                glog.Errorf("Spectrum Scale configuration not found: %v", e)
-                return ScaleSettingsConfigMap{}
-        }
-        cmsj := &ScaleSettingsConfigMap{}
-        e = json.Unmarshal(file, cmsj) 
-        if e != nil {
-                glog.Errorf("Error in unmarshalling Spectrum Scale configuration json: %v", e)
-                return ScaleSettingsConfigMap{}
+	file, e := ioutil.ReadFile(ConfigMapFile) // TODO
+	if e != nil {
+		glog.Errorf("Spectrum Scale configuration not found: %v", e)
+		return ScaleSettingsConfigMap{}
+	}
+	cmsj := &ScaleSettingsConfigMap{}
+	e = json.Unmarshal(file, cmsj)
+	if e != nil {
+		glog.Errorf("Error in unmarshalling Spectrum Scale configuration json: %v", e)
+		return ScaleSettingsConfigMap{}
 	}
 
-        e = HandleSecretsAndCerts(cmsj)
-        if e != nil {
-                glog.Errorf("Error in secrets or certificates: %v", e)
-                return ScaleSettingsConfigMap{}
-        }
-        return *cmsj
+	e = HandleSecretsAndCerts(cmsj)
+	if e != nil {
+		glog.Errorf("Error in secrets or certificates: %v", e)
+		return ScaleSettingsConfigMap{}
+	}
+	return *cmsj
 }
 
 func HandleSecretsAndCerts(cmap *ScaleSettingsConfigMap) error {
-        glog.V(5).Infof("scale_config HandleSecrets")
-        for i := 0; i < len(cmap.Clusters); i++ {
-            if cmap.Clusters[i].Secrets != "" {
-		unamePath := path.Join(SecretBasePath, cmap.Clusters[i].Secrets, "username")
-                file, e := ioutil.ReadFile(unamePath)
-                if e != nil {
-                    return fmt.Errorf("Spectrum Scale secret not found: %v\n", e)
-                }
-		file_s := string(file)
-		file_s = strings.TrimSpace(file_s)
-		file_s = strings.TrimSuffix(file_s, "\n")
-		cmap.Clusters[i].MgmtUsername = string(file_s)
+	glog.V(5).Infof("scale_config HandleSecrets")
+	for i := 0; i < len(cmap.Clusters); i++ {
+		if cmap.Clusters[i].Secrets != "" {
+			unamePath := path.Join(SecretBasePath, cmap.Clusters[i].Secrets, "username")
+			file, e := ioutil.ReadFile(unamePath)
+			if e != nil {
+				return fmt.Errorf("Spectrum Scale secret not found: %v\n", e)
+			}
+			file_s := string(file)
+			file_s = strings.TrimSpace(file_s)
+			file_s = strings.TrimSuffix(file_s, "\n")
+			cmap.Clusters[i].MgmtUsername = string(file_s)
 
-		pwdPath := path.Join(SecretBasePath, cmap.Clusters[i].Secrets, "password")
-                file, e = ioutil.ReadFile(pwdPath)
-                if e != nil {
-                    return fmt.Errorf("Spectrum Scale secret not found: %v\n", e)
-                }  
-                file_s = string(file)
-                file_s = strings.TrimSpace(file_s) 
-		file_s = strings.TrimSuffix(file_s, "\n") 
-		cmap.Clusters[i].MgmtPassword = string(file_s)
-            }
+			pwdPath := path.Join(SecretBasePath, cmap.Clusters[i].Secrets, "password")
+			file, e = ioutil.ReadFile(pwdPath)
+			if e != nil {
+				return fmt.Errorf("Spectrum Scale secret not found: %v\n", e)
+			}
+			file_s = string(file)
+			file_s = strings.TrimSpace(file_s)
+			file_s = strings.TrimSuffix(file_s, "\n")
+			cmap.Clusters[i].MgmtPassword = string(file_s)
+		}
 
-	    if cmap.Clusters[i].SecureSslMode && cmap.Clusters[i].Cacert != "" {
-		certPath := path.Join(CertificatePath, cmap.Clusters[i].Cacert)
-                file, e := ioutil.ReadFile(certPath)
-                if e != nil {
-                    return fmt.Errorf("Spectrum Scale CA certificate not found: %v\n", e)
-                }
-                cmap.Clusters[i].CacertValue = file
-	    }
-        }
-        return nil
+		if cmap.Clusters[i].SecureSslMode && cmap.Clusters[i].Cacert != "" {
+			certPath := path.Join(CertificatePath, cmap.Clusters[i].Cacert)
+			file, e := ioutil.ReadFile(certPath)
+			if e != nil {
+				return fmt.Errorf("Spectrum Scale CA certificate not found: %v\n", e)
+			}
+			cmap.Clusters[i].CacertValue = file
+		}
+	}
+	return nil
 }
-
